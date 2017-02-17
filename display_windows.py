6a14790830de3bfd4f1ec95be4ccb252f380609b
Aleksey <alex_tagan@mail.ru>
2017-02-17 18:17:25 +0000

    I moved to Pygames on wxPython

--- display_windows.py
+++ display_windows.py
@@ -1,26 +1,53 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import pygame
-from pygame.sprite import Group
+# import wx
+#
+# from settings import Settings
+# from button import Button
+# import game_functions as gf
+#
+#
+# class Display_windows(wx.App):
+#     FLTL_settings = Settings()
+#
+#     def OnInit(self):
+#         frame = wx.Frame(parent=None, title="FLTL", size=(self.FLTL_settings.screen_height, self.FLTL_settings.screen_width))
+#         frame.Show()
+#         return True
 
-from settings import Settings
-from button import Button
-import game_functions as gf
+
+import wx
+
+class Frame(wx.Frame):
+    """Frame class that displays an image."""
+
+    def __init__(self, image, parent=None, id=-1,
+            pos = wx.DefaultPosition,
+            title = 'FLTL'):
+
+        """Create a Frame instance and display image."""
+        temp = image.ConvertToBitmap()
+        size = temp.GetWidth(), temp.GetHeight()
+        wx.Frame.__init__(self, parent, id, title, pos, size)
+        self.bmp = wx.StaticBitmap(parent=self, bitmap=temp)
 
 
-class Display_windows():
-    def __init__(self):
-        pygame.init()
-        self.FLTL_settings = Settings()
-        self.screen = pygame.display.set_mode(
-            (self.FLTL_settings.screen_width, self.FLTL_settings.screen_height))
-        pygame.display.set_caption("FLTL")
+class App(wx.App):
+    """Application class."""
 
-    def Main(self):
-        # Make the Play button.
-        self.play_button = Button(self.screen, "Play")
+    def OnInit(self):
+        image = wx.Image('wxPython.jpg', wx.BITMAP_TYPE_JPEG)
+        self.frame = Frame(image)
+        self.frame.Show()
+        self.SetTopWindow(self.frame)
+        return True
 
-    def InLife(self):
-        # Make the Time button.
-        self.time_button = Button(self.screen, "Play")
+
+def main():
+    app = App()
+    app.MainLoop()
+
+if __name__ == '__main__':
+    app = App()
+    app.MainLoop()
--- main_loop.py
+++ main_loop.py
@@ -4,8 +4,10 @@
 import socket
 
 import time
-import pygame
-from pygame.sprite import Group
+# import pygame
+# from pygame.sprite import Group
+import wx
+
 
 from display_windows import Display_windows
 import Config as cfg
@@ -22,60 +24,67 @@ Data = 0
 
 
 def run_game():
-    # Initialize pygame, settings, and screen object.
+    # Initialize settings, and screen object.
     global turn_time
     global Data
 
     start_time = 0
-
     DispWind = Display_windows()
-    DispWind.Main()
+
 
     # Start the main loop for the game.
     while True:
-        screen = pygame.display.set_mode(
-            (Settings().screen_width, Settings().screen_height))
-        # next string get broken display in run game
-        gf.check_events(screen)
-
-        if cfg.check_events().getGameActive() == "True":
-            if start_time == 0:
-                start_time = time.time()
-
-            # Make the Time button.
-            turn_time = int(time.time() - start_time)
-            # print 60 - turn_time
-            print int(turn_time)
-
-            """
-            Need add Time_Button
-            """
-            # time_button = Button(Settings(), DispWind.InLife(), str(60 - turn_time))
-            # time_button.draw_button()
-            pygame.display.flip()
-
-        gf.update_screen(Settings(), DispWind.screen, DispWind.play_button)
-
-        # Every 60 sec to send date server.
-        if turn_time >= Sec_in_one_year:
-            sock = socket.socket()
-            sock.connect(('localhost', PORT))  # Connection to server
-            sock.sendall(str(Data))
-
-            Data = sock.recv(1024)
-
-            print "Age: " + Data
-            turn_time = int(time.clock())
-
-            Data = int(Data)
-            start_time += Sec_in_one_year
-            sock.close()
-
+        print "Bla"
+    #     screen = pygame.display.set_mode(
+    #         (Settings().screen_width, Settings().screen_height))
+    #     # next string get broken display in run game
+    #     gf.check_events(screen)
+    #
+    #     if cfg.check_events().getGameActive() == "True":
+    #         if start_time == 0:
+    #             start_time = time.time()
+    #
+    #         # Make the Time button.
+    #         turn_time = int(time.time() - start_time)
+    #         # print 60 - turn_time
+    #         print int(turn_time)
+    #
+    #         """
+    #         Need add Time_Button
+    #         """
+    #         # time_button = Button(Settings(), DispWind.InLife(), str(60 - turn_time))
+    #         # time_button.draw_button()
+    #         pygame.display.flip()
+    #
+    #     gf.update_screen(Settings(), DispWind.screen, DispWind.play_button)
+    #
+    #     # Every 60 sec to send date server.
+    #     if turn_time >= Sec_in_one_year:
+    #         sock = socket.socket()
+    #         sock.connect(('localhost', PORT))  # Connection to server
+    #         sock.sendall(str(Data))
+    #
+    #         Data = sock.recv(1024)
+    #
+    #         print "Age: " + Data
+    #         turn_time = int(time.clock())
+    #
+    #         Data = int(Data)
+    #         start_time += Sec_in_one_year
+    #         sock.close()
+    #
         time.sleep(time_out)
+        DispWind.MainLoop()
+
 
     print "The client disconnected"
     # return turn_time
 
 
 if __name__ == "__main__":
-    run_game()
+    # run_game()
+    app = wx.PySimpleApp()
+    frame = run_game()
+    frame.Show(True)
+    app.MainLoop()
+
